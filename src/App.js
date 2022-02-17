43cc2ed3d04d3e56ea7dfa9937a20543a79d8694
Sangenis Pablo <sangenispablo@gmail.com>
2022-02-16 23:49:18 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { Gif, Grid } from "@giphy/react-components";
+import ResizeObserver from "react-resize-observer";
 
-function App() {
+const giphyFetch = new GiphyFetch("sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh");
+
+function GridDemo({ onGifClick }) {
+  const fetchGifs = (offset) => giphyFetch.trending({ offset, limit: 10 });
+  const [width, setWidth] = useState(window.innerWidth);
+  console.log(width);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Grid
+        onGifClick={onGifClick}
+        fetchGifs={fetchGifs}
+        width={width}
+        columns={4}
+        gutter={6}
+      />
+      <ResizeObserver
+        onResize={({ width }) => {
+          setWidth(width);
+        }}
+      />
+    </>
   );
 }
 
-export default App;
+export const App = () => {
+  const [modalGif, setModalGif] = useState();
+  return (
+    <>
+      <h2>Official GIPHY React Components</h2>
+      <GridDemo
+        onGifClick={(gif, e) => {
+          console.log("gif", gif);
+          e.preventDefault();
+          setModalGif(gif);
+        }}
+      />
+      {modalGif && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            background: "rgba(0, 0, 0, .8)",
+          }}
+          onClick={(e) => {
+            e.preventDefault();
+            setModalGif(undefined);
+          }}
+        >
+          <Gif gif={modalGif} width={400} />
+        </div>
+      )}
+    </>
+  );
+};
